7d7d808909b05bb83980f6a818453b9ad6523bf0
Vincent Tang <10290348+vincentntang@users.noreply.github.com>
2019-03-17 12:47:43 -0400

    TotalCalls and TotallUsers

--- Website/www/app.js
+++ Website/www/app.js
@@ -1,31 +1,38 @@
 const cafeList = document.querySelector("#cafe-list");
 const form = document.querySelector("#add-cafe-form");
 
+var totalUsers = document.querySelector("#total-users")
+var totalCalls = document.querySelector("#total-calls")
+
+console.log(totalCalls.innerHTML);
+
 function renderCafe(doc) {
-    let li = document.createElement("li");
-    let name = document.createElement("span");
-    let city = document.createElement("span");
-    let cross = document.createElement("div");
-  
-    li.setAttribute("data-id", doc.id); // sets uuid
-    name.textContent = doc.data().age;
-    city.textContent = doc.data().gender;
-  
-    li.appendChild(name);
-    li.appendChild(city);
-    li.appendChild(cross); // This "X" is the delete button
-  
-    cafeList.appendChild(li);
-  
-    // deleting data
-    cross.addEventListener("click", e => {
-      e.stopPropagation(); // optional
-      let id = e.target.parentElement.getAttribute("data-id");
-      db.collection("users")
-        .doc(id)
-        .delete();
-    });
-  }
+  let li = document.createElement("li");
+  let name = document.createElement("span");
+  let city = document.createElement("span");
+  let cross = document.createElement("div");
+
+  console.log(db.collection("users"));
+
+  li.setAttribute("data-id", doc.id); // sets uuid
+  name.textContent = doc.data().age;
+  city.textContent = doc.data().gender;
+
+  li.appendChild(name);
+  li.appendChild(city);
+  li.appendChild(cross); // This "X" is the delete button
+
+  cafeList.appendChild(li);
+
+  // deleting data
+  cross.addEventListener("click", e => {
+    e.stopPropagation(); // optional
+    let id = e.target.parentElement.getAttribute("data-id");
+    db.collection("users")
+      .doc(id)
+      .delete();
+  });
+}
 
 /* CREATE */
 // create element and render cafe
@@ -33,14 +40,18 @@ db.collection("users")
 //   .orderBy("city")
   .onSnapshot(snapshot => {
     let changes = snapshot.docChanges();
-    changes.forEach(change => {
-      if (change.type == "added") {
-        renderCafe(change.doc);
-      } else if (change.type == "removed") {
-        let li = cafeList.querySelector("[data-id=" + change.doc.id + "]");
-        cafeList.removedChild(li);
-      }
-    });
+
+    console.log(changes.length)
+    totalUsers.innerHTML = changes.length
+
+    // changes.forEach(change => {
+    //   if (change.type == "added") {
+    //     renderCafe(change.doc);
+    //   } else if (change.type == "removed") {
+    //     let li = cafeList.querySelector("[data-id=" + change.doc.id + "]");
+    //     cafeList.removedChild(li);
+    //   }
+    // });
   });
 
   /* saving data */
@@ -53,3 +64,40 @@ db.collection("users")
 //     form.name.value = "";
 //     form.city.value = "";
 //   });
+
+
+db.collection("CallLogger")
+  //   .orderBy("city")
+  .onSnapshot(snapshot => {
+  let changes = snapshot.docChanges();
+  console.log(changes.length)
+  totalCalls.innerHTML = changes.length
+});
+
+
+
+// function renderCafe(doc) {
+//   let li = document.createElement("li");
+//   let name = document.createElement("span");
+//   let city = document.createElement("span");
+//   let cross = document.createElement("div");
+
+//   li.setAttribute("data-id", doc.id); // sets uuid
+//   name.textContent = doc.data().age;
+//   city.textContent = doc.data().gender;
+
+//   li.appendChild(name);
+//   li.appendChild(city);
+//   li.appendChild(cross); // This "X" is the delete button
+
+//   cafeList.appendChild(li);
+
+//   // deleting data
+//   cross.addEventListener("click", e => {
+//     e.stopPropagation(); // optional
+//     let id = e.target.parentElement.getAttribute("data-id");
+//     db.collection("CallLogger")
+//       .doc(id)
+//       .delete();
+//   });
+// }
