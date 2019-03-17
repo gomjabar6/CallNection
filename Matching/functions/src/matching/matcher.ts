b4571cba80b0d1795b2333490e2e7594d207b57a
Garrett S. Curtis <garrett@cyber-medics.com>
2019-03-16 20:44:37 -0400

    Matching Changes

---
+++ Matching/functions/src/matching/matcher.ts
@@ -0,0 +1,11 @@
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
+
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+
+export function Match(){
+    
+    
+
+}
